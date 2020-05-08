17301c7b58198963c27b05185bf8d4b2770ce91d
mashiranna <55777589+mashiranna@users.noreply.github.com>
2020-05-08 19:49:57 +0300

    Update asd.py
    Масив задається прямо в тексті коду та знаходиться не поза функцією.
    Прибране добування кореня.

--- asd.py
+++ asd.py
@@ -1,27 +1,22 @@
 import math
 def find_min_distance():
+ 
+  # n - кількість точок, k - розмірність простору
+  # В масив arr потрібно ввести координати точок.
+
+  arr = [[1, 5, 7], [3, 4, 8], [1, 6, 5], [-1, -5, 0]]
+  n = len(arr)
+  distance = 0
   min_distance = math.inf
-  for i in range(k-1):
-    for j in range(i+1, k):
-      sub = 0
-      for a in range(n):
-        sub += (arr[i][a] - arr[i+1][a])**2
+  for i in range(n-1):
+    k = len(arr[i])
+    for j in range(i+1, n):
+      for a in range(k):
+        distance += (arr[i][a] - arr[i+1][a])**2      
+        if distance < min_distance:
+          min_distance = distance
+          points = [i, i+1]
+  print(arr)        
+  print("\nМінімальна відстань між точками номер ", points[0], " та ", points[1])
 
-      distance = math.sqrt(sub)
-      if distance < min_distance:
-        min_distance = distance
-        points = [i, i+1]
-  print('\nМінімальна відстань: ', min_distance, "\nМіж точками номер ", points[0], " та ", points[1])
-
-n = int(input("Введіть розмірність простору "))
-k = int(input("Введіть кількість точок "))
-arr = []
-for i in range(k):
-  cords = []
-  while len(cords) != n:
-    print("Введіть координати точки ", i+1, " через пробіл:")
-    cords = input().split(" ")
-    cords_int = [int(i) for i in cords]
-  arr.append(cords_int)
-print(arr)
 find_min_distance()
